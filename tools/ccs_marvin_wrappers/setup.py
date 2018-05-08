b99d61c29c024965c4e1714d84a79a132f5652f6
henko holtzhausen <henko.holtzhausen@shapeblue.com>
2018-05-08 12:47:30 +0200

    packaged marvin wrapper files to use with pip install

---
+++ tools/ccs_marvin_wrappers/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ccs_marvin_wrappers',
+        version='0.1',
+        description='Wrappers for running CCS marvin tests',
+        author='dhlaluku',
+        author_email='dingane.hlaluku@shapeblue.com',
+        license='http://www.apache.org/licenses/LICENSE-2.0',
+        packages=['ccs_marvin_wrappers'],
+        zip_safe=False)
