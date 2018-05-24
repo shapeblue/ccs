6ae3caf7fdf049f9e1f45b5aedf774850c78de04
henko holtzhausen <henko.holtzhausen@shapeblue.com>
2018-05-24 16:28:18 +0200

    changed author name and email

--- tools/ccs_marvin_wrappers/setup.py
+++ tools/ccs_marvin_wrappers/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(name='marvin.ccsAPI',
         version='0.1',
         description='Wrappers for running CCS marvin tests',
-        author='dhlaluku',
-        author_email='dingane.hlaluku@shapeblue.com',
+        author='ShapeBlue',
+        author_email='engineering@shapeblue.com',
         license='http://www.apache.org/licenses/LICENSE-2.0',
         packages=['marvin.ccsAPI'],
         zip_safe=False)
