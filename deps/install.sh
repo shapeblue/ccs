9cbeef196ee9a115f1252c1d57d9bae7701a34b1
Rohit Yadav <rohit@apache.org>
2018-10-05 00:46:29 +0530

    add missing dependency
    Signed-off-by: Rohit Yadav <rohit@apache.org>

--- deps/install.sh
+++ deps/install.sh
@@ -14,7 +14,7 @@
 #!/bin/bash
 
 version="4.11.1.0"
-for dep in checkstyle cloud-api cloud-utils cloud-server cloud-core cloud-engine-components-api cloud-engine-orchestration cloud-engine-api cloud-engine-schema cloud-framework-config cloud-framework-db cloud-framework-managed-context cloud-framework-security; do
+for dep in checkstyle cloud-api cloud-utils cloud-server cloud-core cloud-engine-components-api cloud-engine-orchestration cloud-engine-api cloud-engine-schema cloud-framework-ca cloud-framework-config cloud-framework-db cloud-framework-managed-context cloud-framework-security; do
     echo "Installing $dep"
     mvn -q install:install-file -Dfile=$dep-$version.jar -DgroupId=org.apache.cloudstack -DartifactId=$dep   -Dversion=$version   -Dpackaging=jar
 done
